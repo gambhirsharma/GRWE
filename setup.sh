926d2a98610b53c4e0291411323045f8bd196bdb
gambhir ⚡ <gambhir786191ss@gmail.com>
2024-08-10 12:55:43 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define your GitHub repository URL
-CONFIG_REPO="https://github.com/yourusername/configs.git"
+CONFIG_REPO="https://github.com/gambhirsharma/GRWE.git"
 
 # Clone the repository to a temporary directory
 TEMP_DIR=$(mktemp -d)
@@ -10,18 +10,19 @@ git clone "$CONFIG_REPO" "$TEMP_DIR"
 # Backup and move Neovim configuration
 if [ -d "$HOME/.config/nvim" ]; then
     mv "$HOME/.config/nvim" "$HOME/.config/nvim.bak"
+    mkdir -p "$HOME/.config/nvim"
 else
     mkdir -p "$HOME/.config/nvim"
 fi
-mv "$TEMP_DIR/nvim" "$HOME/.config/nvim"
+mv "$TEMP_DIR/nvim/" "$HOME/.config/"
+
 
 # Backup and move Tmux configuration
 if [ -f "$HOME/.tmux.conf" ]; then
     mv "$HOME/.tmux.conf" "$HOME/.tmux.conf.bak"
-else
-    touch "$HOME/.tmux.conf"
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
-mv "$TEMP_DIR/tmux/.tmux.conf" "$HOME/.tmux.conf"
+mv "$TEMP_DIR/.tmux.conf" "$HOME/.tmux.conf" 
 
 # Clean up the temporary directory
 rm -rf "$TEMP_DIR"
